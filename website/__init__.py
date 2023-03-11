f7658a7ee2a4fa0ea5f15c31f00c2cf1e261d39e
toXCustom <pawel89@gmail.com>
2023-03-11 23:46:29 +0100

    added database creation with Flask

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -12,7 +13,13 @@ def create_app():
 
     from .views import views
     from .auth import auth
+
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
 
     return app
