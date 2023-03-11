aac5956784d5d5a0028b996d6b0fb2fc4660ed60
toXCustom <pawel89@gmail.com>
2023-03-12 00:16:28 +0100

    Flask Login Module

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -10,6 +11,10 @@ def create_app():
     app.config['SECRET_KEY'] = 'toxcustomfirstpythonwebsite'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -21,5 +26,9 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
